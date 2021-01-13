a7902cf06f5c5f2f036777293f20dd882074786f
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-13 22:55:12 +0000

    added controls

--- play.js
+++ play.js
@@ -11,4 +11,36 @@ const game = new Game(new UserInterface(), new RemoteInterface());
 game.start();
 
 console.log('Connecting ...');
-connect();
+const conn = connect();
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+let currentDirection = "";
+const input = setupInput();
+
+input.on('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === "w" && currentDirection !== "Move: down") {
+    currentDirection = "Move: up";
+  } else if (key === "s" && currentDirection !== "Move: up") {
+    currentDirection = "Move: down";
+  } else if (key === "a" && currentDirection !== "Move: right") {
+    currentDirection = "Move: left";
+  } else if (key === "d" && currentDirection !== "Move: left") {
+    currentDirection = "Move: right";
+  }
+
+});;
+
+setInterval(() => {
+  conn.write(currentDirection);
+}, 60);
