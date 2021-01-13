e892eeda2b37bc0af2fd724ad7769d00aa5ec68c
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-13 21:33:09 +0000

    factored out the client logic

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const net = require('net');
+const { connect } = require('./client');
 process.stdout.write('\x07');
 
 const { Game } = require('./src/Game');
@@ -9,23 +9,6 @@ const game = new Game(new UserInterface(), new RemoteInterface());
 
 // Begin game
 game.start();
-
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-    // port: 50541host
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
 
 console.log('Connecting ...');
 connect();
