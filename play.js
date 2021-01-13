37b88d708ea732fe0707e431ae2bac480d8d1245
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-13 21:29:33 +0000

    added connection details for server

--- play.js
+++ play.js
@@ -1,11 +1,31 @@
 #!/usr/bin/env node
-
+const net = require('net');
 process.stdout.write('\x07');
 
-const { Game } = require('./src/Game')
-const { UserInterface } = require('./src/UserInterface')
-const { RemoteInterface } = require('./src/RemoteInterface')
-const game = new Game(new UserInterface(), new RemoteInterface())
+const { Game } = require('./src/Game');
+const { UserInterface } = require('./src/UserInterface');
+const { RemoteInterface } = require('./src/RemoteInterface');
+const game = new Game(new UserInterface(), new RemoteInterface());
 
 // Begin game
-game.start()
+game.start();
+
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+    // port: 50541host
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
